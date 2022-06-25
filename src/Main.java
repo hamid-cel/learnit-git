b80c84134c5c82856910486e56e73da57d76ced3
hamid <mahmoudchakroun38@gmail.com>
2022-06-25 17:47:24 +0100

    add array to main

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         for(int i=0;i<10;i++){
             System.out.println("this is a loop");
         }
+        int[] arr ={0,1,3,4,5,5};
+
+        for (int i=0;i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
 
 
 
